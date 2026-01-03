b7f80ec2e8e243d94cda86a4435b756f182873a4
AlicjaK-S <alicja.kubotstarkiewicz@gmail.com>
2026-01-03 10:26:08 +0000

    Added imie to Czlowiek constructor

--- klasy.py
+++ klasy.py
@@ -1,22 +1,25 @@
 #Klasa = Szablon, Przepis
 class Czlowiek:
     gatunek = "Homo Sapiens"
-    def __init__(self):
-        print("Niech powstanie Czlowiek")
-
-
+    def __init__(self, imie):
+        print(f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
+        # to jest to samo jakbym wpisala adam.imie = "Adam"
+        #ewa.imie = "Ewa"
 
 # print(Czlowiek)
 # print(type(Czlowiek))
 # print(dir(Czlowiek))
-#
 # #jak mam podloge __ tzn ze juz jest zdefiniowane przez pythona tzw magia pythona
 # print(__name__)
 
+#postanie obiektu, Gotowanie z przepisu
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
+# print(type(adam))
+# print(dir(adam)
 
-#postanie obiegtu, Gotowanie z przepisu
-ala = Czlowiek()
-# print(type(ala))
-# print(dir(ala))
-
-print(ala.gatunek)
+print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
